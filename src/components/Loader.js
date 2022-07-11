cfd2f3bbce6f321790656b42ebdb461cbb097772
gazzaraki <95699162+gazzaraki@users.noreply.github.com>
2022-07-11 11:33:47 -0500

    first commit

---
+++ src/components/Loader.js
@@ -0,0 +1,21 @@
+const Loader = () => {
+  return (
+    <div id="loading" class="container" style={{ height: '100vh', maxWidth: '100%' }}>
+      <div class="row justify-content-center align-items-center">
+        <div class="col-12"
+             style={{ marginTop: '45vh', marginLeft: '85vw'}}>
+          <lottie-player 
+            src="https://assets6.lottiefiles.com/packages/lf20_bxw59kpa.json"
+            background="transparent"
+            speed="1" 
+            style={{ width: '13vmin', height: '13vmin'}}
+            loop
+            autoplay>
+          </lottie-player>
+        </div>
+      </div>
+    </div>
+  )
+} 
+
+export default Loader
---
+++ src/components/SendForm.js
@@ -0,0 +1,82 @@
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+
+const SendForm = () => {
+  
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('service_dlf32ye', 'template_mailer4all', form.current, 'yU_ce4p04RlrLWunW')
+      .then((result) => {
+          alert('Message sent succesfully')
+      }, (error) => {
+          alert(error.message)
+      })
+      e.target.reset()
+  }
+
+  return (
+
+    <form id='allform' ref={form} onSubmit={sendEmail} class="row justify-content-center">
+        
+        {/* U S E R  N A M E */}
+        <div class="row justify-content-center">
+          <div class="col-10 mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Name</label>
+            <input type="text" name="user_name" class="form-control" id="exampleFormControlInput1" placeholder="Your name" required={true}/>
+          </div>
+        </div>
+
+        {/* F R O M  E M A I L */}
+        <div class="row justify-content-center">
+          <div class="col-10 mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Your Email</label>
+            <input type="email" name="user_email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required={true}/>
+          </div>
+        </div>
+
+        {/* T O  E M A I L */}
+        <div class="row justify-content-center">
+          <div class="col-10 mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email destiny</label>
+            <input type="email" name="to_email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required={true}/>
+          </div>
+        </div>
+
+        {/* T E X T  A R E A */}
+        <div class="row justify-content-center">
+          <div class="col-10 mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+            <textarea 
+              class="form-control" 
+              id="exampleFormControlTextarea1" 
+              name="message"
+              rows="5" 
+              placeholder="Write something..."
+              required={true}
+              style={{ resize: 'none' }}>
+            </textarea>
+          </div>
+        </div>
+
+        {/* B U T T O N */}
+        <div class="row justify-content-center">
+          <button 
+            class="col-3 mt-1 btn btn-primary"
+            type="submit"
+            value="Send"
+            id="sendFormButton"
+            style={{ }}>
+            Send
+          </button>
+        </div>
+          
+      </form>
+
+  )
+}
+
+export default SendForm
+      
