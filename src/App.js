f4e077a74ffdc6f05a5ed769dfcbb3bf3bcd0bd5
gazzaraki <95699162+gazzaraki@users.noreply.github.com>
2022-07-11 11:32:22 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Suspense } from 'react'
 import './App.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+AOS.init()
+
+const Loader = React.lazy(() => import("./components/Loader"))
+const SendForm = React.lazy(() => import("./components/SendForm"))
+
+const App = () => {
+
+  return  (
+
+    <div
+      id='all'
+      class="container"
+      style={{ background: '#ededed', width:'100%', margin: 0 }}>
+      
+      <Suspense id='all' fallback={<Loader id='all' />}>
+        <div class='row'>
+        
+          {/* C O M P O N E N T S  S T A R T */}
+        
+          <SendForm/>
+
+          {/* C O M P O N E N T S  E N D */}
+        
+        </div>
+      </Suspense>
+    
     </div>
-  );
+
+  )
+  
 }
 
-export default App;
+export default App
